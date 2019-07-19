f2e6290d8e661777f677729718132e97b8e3ea61
unknown <2469225840@qq.com>
2019-07-19 14:43:50 +0800

    完成footGuide

--- src/components/FootGuide/FootGuide.vue
+++ src/components/FootGuide/FootGuide.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="footGuide">
-     <div class="footGuide-item" @click="goto('/home')">
+     <div class="footGuide-item" :class="{on:this.$route.path=='/home'}" @click="goto('/home')">
         <div class="icon-wrap">
           <i class="iconfont icon-waimai"></i>
         </div>
         <span>首页</span>
 	 </div>
-	 <div class="footGuide-item" @click="goto('/search')">
+	 <div class="footGuide-item" :class="{on:this.$route.path=='/search'}" @click="goto('/search')">
         <div class="icon-wrap">
           <i class="iconfont icon-search"></i>
         </div>
         <span>搜索</span>
 	 </div>
-	 <div class="footGuide-item" @click="goto('/order')">
+	 <div class="footGuide-item" :class="{on:this.$route.path=='/order'}" @click="goto('/order')">
         <div class="icon-wrap">
           <i class="iconfont icon-dingdan"></i>
         </div>
         <span>订单</span>
 	 </div>
-	 <div class="footGuide-item" @click="goto('/profile')">
+	 <div class="footGuide-item" :class="{on:this.$route.path=='/profile'}" @click="goto('/profile')">
         <div class="icon-wrap">
           <i class="iconfont icon-geren"></i>
         </div>
@@ -50,6 +50,7 @@ export default {
 <style lang="stylus" rel="stylesheet/stylus">
   @import '../../common/styles//mixins.styl'
   .footGuide
+    color #999999
     border-top 1px solid #f3f3f3
     position fixed
     left 0
@@ -66,6 +67,8 @@ export default {
       flex-direction column
       line-height .2rem
       flex 1
+      &.on
+        color #02a774
       .iconfont
         font-size .22rem
         
